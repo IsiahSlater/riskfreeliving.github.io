db35f440323b331e325ec2a02404a347eed090a8
Isslater182 <mr.bwee@gmail.com>
2019-10-12 21:21:30 -0500

    It works!!
    Html - implimented embedded javascript that generates a mapbox displaying the pollution rates in varying cities.
    CSS - edit display overlays to hold data

--- Main.js
+++ Main.js
@@ -2,6 +2,6 @@ function loadMap(){
   mapboxgl.accessToken = 'pk.eyJ1IjoiaXNzbGF0ZXIxODIiLCJhIjoiY2sxbnIwM2ZrMGVnYzNwcGtqN2xtMTVkbyJ9.jV4Fno7zzYI-HWSxygDdUg';
   var map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11'
+    style: 'mapbox://styles/isslater182/ck1nsy8vj169z1cqa977byyf3'
   });
 }
